d2cb8672fc43d71ef4fcfdfe0d0b0b12c911e153
Stalawatt <joshuamrowell@gmail.com>
2024-12-12 09:31:12 +0000

    Added memory access to declaration value

--- Rahhhsembly/Interpreter.py
+++ Rahhhsembly/Interpreter.py
@@ -1,42 +1,42 @@
-import sys, re
+import sys
+import re
 
-class DATAMEMORY :
-    def __init__(self, length:int):
+
+class DATAMEMORY:
+    def __init__(self, length: int):
         """
-        
+
         Data Memory for the program, each index in memory has another array of length 2 that is in format [value, type]
-        
+
         Methods :
         .get_value(index) : returns value at the given index
         .get_type(index) : returns type at the given index
         .set_value(index, value) : if value is of correct type, sets the value at index given to value given
 
         """
-        self.length : int = int(length) # this is the length of the array
-        self.__MEMORYARRAY : list[object,type]= [[None,None]] * self.length # generate the array that starts as [None,None] for every index in the array
+        self.length: int = int(length)  # this is the length of the array
+        # generate the array that starts as [None,None] for every index in the array
+        self.__MEMORYARRAY: list[object, type] = [[None, None]] * self.length
 
-    def get_value(self, index:int):
+    def get_value(self, index: int):
         self.__val_index(index)
-        return self.__MEMORYARRAY[index]
-    
-    
-    
-    def set_value(self, index:int, value:int|str|float):
+        return self.__MEMORYARRAY[int(index)]
+
+    def set_value(self, index: int, value: int | str | float):
         self.__val_index(index)
 
-        
         self.__MEMORYARRAY[index] = value
         return
-    
-    def declare(self, index:int, value:int|str|float):
+
+    def declare(self, index: int, value: int | str | float):
         self.__val_index(index)
         self.__MEMORYARRAY[index] = value
         return
-    
-    def __val_index(self, index:int):
-        if index > self.length-1:
+
+    def __val_index(self, index: int):
+        if int(index) > self.length-1:
             return ValueError("Index out of range")
-    
+
 
 class Instr:
     def __init__(self):
@@ -57,17 +57,18 @@ class Instr:
         These 10 instructions make a turing complete machine
         """
 
-        self.data : DATAMEMORY = None
-        self.register : int|str|float = None
-        self.types = {"int" : int, "str" : str, "float" : float}
+        self.data: DATAMEMORY = None
+        self.register: int | str | float = None
+        self.types = {"int": int, "str": str, "float": float}
 
     def HALT(self):
         sys.exit(0)
 
-    def ALLOCATE(self,length:int):
+    def ALLOCATE(self, length: int):
         self.data = DATAMEMORY(length)
 
-    def DECLARE(self,index:int,value:str):
+    def DECLARE(self, index: int, value: str):
+        value = str(value)
         if value.isdigit():
             inferred_value = int(value)
         else:
@@ -75,38 +76,39 @@ class Instr:
                 inferred_value = float(value)
             except ValueError:
                 inferred_value = value
-        self.data.declare(int(index),inferred_value)
+        self.data.declare(int(index), inferred_value)
 
-    def LOAD_INDEX(self,index:int):
+    def LOAD_INDEX(self, index: int):
         self.register = self.data.get_value(int(index))
-    
-    def LOAD_VALUE(self,value:str):
+
+    def LOAD_VALUE(self, value: str):
         self.register = value
 
-    def STORE(self, index:int):
-        self.data.set_value(int(index),self.register)
-    
+    def STORE(self, index: int):
+        self.data.set_value(int(index), self.register)
+
     def PRINT(self):
         print(self.register)
 
-    def ADD_INDEX(self, index:int):
+    def ADD_INDEX(self, index: int):
         self.register += self.data.get_value(int(index))
 
-    def ADD_VALUE(self,value:str):
+    def ADD_VALUE(self, value: str):
         self.register += value
 
-    def SUB_INDEX(self, index:int):
+    def SUB_INDEX(self, index: int):
         self.register -= self.data.get_value(int(index))
 
-    def SUB_VALUE(self,value:str):
+    def SUB_VALUE(self, value: str):
         self.register -= value
 
-    def JUMPIF(self, index : int, condition:str):
+    def JUMPIF(self, index: int, condition: str):
         pattern = r"&(\d+)"
+
         def replace_memory(match):
             variable = int(match.group(1))
             return str(self.data.get_value(variable))
-        if eval(re.sub(pattern,replace_memory,condition)):
+        if eval(re.sub(pattern, replace_memory, condition)):
             interpreter.currentline = int(index)-2
 
 
@@ -116,9 +118,8 @@ class Parser:
         Parser for interpreting each line and what to do
         """
         self.instructions = Instr()
-        
 
-    def parseLine(self,line:str):
+    def parseLine(self, line: str):
         if "#" in line:
             return
         line = line.rstrip("\n")
@@ -131,14 +132,20 @@ class Parser:
                 case "DECLARE":
                     if "&" in line[1]:
                         line[1] = line[1].lstrip("&")
-                        
-                    self.instructions.DECLARE(line[1],line[2])
+
+                    if "&" in line[2]:
+                        self.instructions.DECLARE(
+                            line[1], self.instructions.data.get_value(line[2].lstrip("&")))
+                    else:
+                        self.instructions.DECLARE(line[1], line[2])
                 case "LOAD":
                     if "&" in line[1]:
                         self.instructions.LOAD_INDEX(line[1].lstrip("&"))
                     else:
                         self.instructions.LOAD_VALUE(line[1])
                 case "STORE":
+                    if "&" in line[1]:
+                        line[1] = line[1].lstrip("&")
                     self.instructions.STORE(line[1])
                 case "ADD":
                     if "&" in line[1]:
@@ -152,34 +159,28 @@ class Parser:
                         self.instructions.SUB_VALUE(line[1])
                 case "JUMP":
                     interpreter.currentline = int(line[1])-2
-                
-                case "JUMPIF" :
+
+                case "JUMPIF":
                     self.instructions.JUMPIF(line[1], "".join(line[1:]))
 
-                    
-
-                
-        else :
+        else:
             match line:
-                case "HALT" :
+                case "HALT":
                     self.instructions.HALT()
-                case "PRINT" :
+                case "PRINT":
                     self.instructions.PRINT()
-
-            
 
 
 class Interpreter:
-    def __init__(self,file):
+    def __init__(self, file):
         self.file = list(file)
         self.currentline = 0
         self.parser = Parser()
-        
 
     def nextLine(self):
         self.parser.parseLine(self.file[self.currentline])
         self.currentline += 1
-        
+
 
 def begin_program(file):
     global interpreter
